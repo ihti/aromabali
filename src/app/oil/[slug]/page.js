08f7a1ead55f49416e97a7d91061be511da919d8
ihti <jasminihtijarevic@gmail.com>
2024-07-04 23:47:07 +0800

    fix

--- src/app/oil/[slug]/page.js
+++ src/app/oil/[slug]/page.js
@@ -1,20 +1,36 @@
-import oils from "../../data/oils.json"
+import Image from "next/image";
+import Link from "next/link";
+import oils from "../../data/oils.json";
 
 export default function Page({ params }) {
-  const oil = oils.find((oil) => oil.url === params.slug)
+  const oil = oils.find((oil) => oil.url === params.slug);
   if (!oil) {
-    return <div>Oil not found</div>
+    return <div>Oil not found</div>;
   }
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen text-center text-white" style={{ backgroundColor: oil.color_code }}>
-      <h1 className="text-6xl mb-8">{oil.name}</h1>
-      <h2 className="text-4xl mb-4">Ingredients:</h2>
-      <ul className="list-none p-0">
-        {oil.ingredients.map((ingredient, index) => (
-          <li key={index} className="text-2xl mb-2">{ingredient}</li>
-        ))}
-      </ul>
+    <div
+      className="flex flex-col items-center justify-center min-h-screen text-center text-white"
+      style={{ backgroundColor: oil.color_code }}
+    >
+      <div className="absolute top-4 left-4">
+        <a href="/" className="text-white underline">
+          Home
+        </a>
+      </div>
+      <div className="w-full mb-4 sm:mb-0 flex justify-center bg-white">
+        <Link href="/">
+          <Image
+            src="/logo.png"
+            alt="Aroma Bali"
+            width={1450}
+            height={756}
+            priority
+            className="w-[240px] h-auto sm:w-[480px] sm:h-auto"
+          />
+        </Link>
+      </div>
+      <h1 className="text-5xl mt-8 uppercase">{oil.name}</h1>
     </div>
-  )
+  );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import oils from "./data/oils.json";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -17,9 +18,11 @@ export default function Home() {
       
       <div className="flex flex-wrap justify-center space-x-2 sm:space-x-4">
         {oils.map((oil) => (
-          <div key={oil.url} className="rounded-full p-2 px-4 mb-2 sm:mb-0" style={{ backgroundColor: oil.color_code }}>
-            <h2 className="text-xs sm:text-sm text-gray-50 font-medium uppercase">{oil.name}</h2>
-          </div>
+          <Link key={oil.url} href={`/oil/${oil.url}`}>
+            <div  className="rounded-full p-2 px-4 mb-2 sm:mb-0" style={{ backgroundColor: oil.color_code }}>
+              <h2 className="text-xs sm:text-sm text-gray-50 font-medium uppercase">{oil.name}</h2>
+            </div>
+          </Link>
         ))}
       </div>
     </main>
