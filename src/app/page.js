7c6d02c42c2b3a949a79d185cfe945da3e8d03d1
ihti <jasminihtijarevic@gmail.com>
2024-07-08 16:22:52 +0800

    fix

--- src/app/page.js
+++ src/app/page.js
@@ -15,12 +15,17 @@ export default function Home() {
           className="w-[240px] h-auto sm:w-[480px] sm:h-auto"
         />
       </div>
-      
+
       <div className="flex flex-wrap justify-center space-x-2 sm:space-x-4">
         {oils.map((oil) => (
-          <Link key={oil.url} href={`/oil/${oil.url}`}>
-            <div  className="rounded-full p-2 px-4 mb-2 sm:mb-0" style={{ backgroundColor: oil.color_code }}>
-              <h2 className="text-xs sm:text-sm text-gray-50 font-medium uppercase">{oil.name}</h2>
+          <Link key={oil.url} href={`/massage-oil/${oil.url}`}>
+            <div
+              className="rounded-full p-2 px-4 mb-2 sm:mb-0"
+              style={{ backgroundColor: oil.color_code }}
+            >
+              <h2 className="text-xs sm:text-sm text-gray-50 font-medium uppercase">
+                {oil.name}
+              </h2>
             </div>
           </Link>
         ))}
